a5c0e70a3304f511cfe9eccf30ae13c92cc363cd
Matulis <70170180+LucasMatulis@users.noreply.github.com>
2023-11-09 15:03:00 -0300

    ad

--- site/src/pages/edicao/index.js
+++ site/src/pages/edicao/index.js
@@ -7,9 +7,16 @@ import axios from 'axios';
 
 function Edicao(){
 
-    const [listaJogos, setListaJogos] = useState([]);
-    const [imagem, setImagem] = useState({});
+    const [listaJogos, setListaJogos] = useState([]); 
+    const [nomeJogo, setNomeJogo]=useState("");
+    const [precoJogo, setPrecoJogo]=useState("")
+    const [selectedImage, setSelectedImage] = useState("/assets/images/branco.png");
 
+
+        const handleImageSelect = (e) => {
+            const file = e.target.files[0];
+            setSelectedImage(URL.createObjectURL(file));
+        };
 
         async function buscarJogo() {
             let r = await axios.get('http://localhost:5000/jogo');
@@ -20,6 +27,55 @@ function Edicao(){
 
     
         }
+
+        async function atualizarJogo(item) {
+
+            if(!nomeJogo & !precoJogo & !selectedImage){
+                alert("Nenhuma alteração feita")
+            }
+
+            let jogoAtualizado = {
+              id: item.id,
+              nomeJogo: nomeJogo, 
+              precoJogo: Number(precoJogo),
+              imagemJogo:selectedImage
+            
+            };
+          
+            try {
+              let response = await axios.put(`http://localhost:5000/jogo/${jogoAtualizado.id}`, jogoAtualizado);
+              alert("Jogo Alterado. Id: " + item.id);
+              window.location.reload(false)
+            } catch (error) {
+              console.error("Erro ao atualizar jogo:", error);
+              console.log(`http://localhost:5000/jogo/${jogoAtualizado.id}`)
+            }
+          }
+
+          async function deletarJogo(id){
+
+            var resultado = window.confirm("Deseja excluir o item ?");
+            if (resultado == true) {
+
+                const idInfo=id
+
+                try {
+                        let response = await axios.delete(`http://localhost:5000/jogo/${id}`);
+                        alert("Jogo Deletado. Id: " + idInfo);
+                        window.location.reload(false)
+                    } catch (error) {
+                        console.error("Erro ao deletar jogo:", error);
+                        console.log(`http://localhost:5000/jogo/${id}`)
+                }           
+            }
+                else{
+                    return;
+            }
+
+
+          }
+
+
     
         useEffect(() => {
         
@@ -46,17 +102,17 @@ function Edicao(){
                 {listaJogos.map(item=>
                 <div className="cadastro" key={item.id}>
                     <div className="imagem">
-                        <img src={item.imageUrl} className="stock"/>
-                        <input type="file"  accept="image/*" className="link"/>                    
+                    {selectedImage &&<img src={selectedImage} className="stock"/>}
+                                <input type="file"  accept="image/*" className="link" onChange={handleImageSelect}/>                   
                     </div>
                     <div className="inputs">
                         <label for="">Inserir Nome:</label>
-                        <input type="text" className="Nome" placeholder={item.nome} />
+                        <input type="text" className="Nome" placeholder={item.nome} onChange={(e)=>setNomeJogo(e.target.value)} />
                         <label for="">Inserir Preço:</label>
-                        <input type="text" className="Nome" placeholder={item.preco} />
+                        <input type="text" className="Nome" placeholder={item.preco} onChange={(e)=>setPrecoJogo(e.target.value)} />
                         <div>
-                            <input className="botao" type="button" value="Editar"/>
-                            <input className="botao" type="button" value="Deletar"/>
+                            <button className="botao" type="button" onClick={() =>atualizarJogo(item)} >EDITAR</button>
+                            <button className="botao" type="button" onClick={() =>deletarJogo(item.id)} >DELETAR</button>
                         </div>
                     </div>
                 </div>)}
