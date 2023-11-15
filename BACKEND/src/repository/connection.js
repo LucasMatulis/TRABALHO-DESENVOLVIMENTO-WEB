f1aa80eb6246b5132322dab496c6e49babf11c65
Matulis <70170180+LucasMatulis@users.noreply.github.com>
2023-11-15 16:46:00 -0300

    sadasd

--- BACKEND/src/repository/connection.js
+++ BACKEND/src/repository/connection.js
@@ -1,11 +1,12 @@
 import mysql2 from 'mysql2/promise';
 
 
+
 const con = await mysql2.createConnection({
-    host: process.env.HOST,
-    database: process.env.DB,
-    user: process.env.USER,
-    password: process.env.PWD
+    host: process.env.MYSQL_HOST,
+    database: process.env.MYSQL_DB,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PWD
 })
 
 
