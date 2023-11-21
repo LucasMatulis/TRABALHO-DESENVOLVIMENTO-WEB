21943a2e5bd6041440acb291f8442660d02517cc
Matulis <70170180+LucasMatulis@users.noreply.github.com>
2023-11-21 11:04:56 -0300

    ad

--- site/src/pages/app/index.js
+++ site/src/pages/app/index.js
@@ -1,11 +1,14 @@
 import { Link } from 'react-router-dom';
 import './index.scss';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef  } from 'react';
 import axios from 'axios';
+import Swiper from 'swiper';
+
 
 function App() {
   const [listaJogos, setListaJogos] = useState([]);
   const [termoPesquisa, setTermoPesquisa] = useState('');
+  const swiperRef = useRef(null);
 
   async function buscarJogo() {
     let r = await axios.get('http://20.197.242.211:5000/jogo');
@@ -15,6 +18,8 @@ function App() {
 
   useEffect(() => {
     buscarJogo();
+
+    
   }, []);
 
   const handlePesquisa = (event) => {
@@ -41,43 +46,30 @@ function App() {
         </Link>
       </header>
 
-      
-      <div>
-        <ul>
-          {jogosFiltrados.map((item) => (
-            <li key={item.id}>
-              <div className="jogo">
-                <img src={`http://20.197.242.211:5000/${item.imagem}`} className="stock" alt="Imagem do Jogo"/>
-                <h1 className="nome">{item.nome}</h1>
-                <h1 className="preco">R$ {item.preco}</h1>
-              </div>
-            </li>
-          ))}
-        </ul>
-        </div>
-      
-      <body className='home'>
-      <div class="slider-container">
-        <div class="slide-content">
-            <div class="card-wrapper">
-                 <div class="card">
-                    <div class="image-content">
-                        <span class="overlay"></span>
+        <body className='home'>
+        {jogosFiltrados.map((item) => (
+              <div class="slider-container">
+              <div class="slide-content">
+                  <div class="card-wrapper">
+                      <div class="card">
+                          <div class="image-content">
+                              <span class="overlay"></span>
 
-                         <div class="card-image">
-                            <img src="/assets/images/metalslug.webp" alt="" class="card-img"/>
-                        </div>
-                    </div>
-                    <div class="card-content">
-                    <h1 class="name">metal slug</h1>
-                    <h2 class="precoo">R$ 10 </h2>
-                    </div>
-                 </div>
-            </div>
-         </div>
-      </div>
-   
-      </body>
+                              <div class="card-image">
+                                  <img src={`http://20.197.242.211:5000/${item.imagem}`} alt="" class="card-img"/>
+                              </div>
+                          </div>
+                          <div class="card-content">
+                          <h1 class="name">{item.nome}</h1>
+                          <h2 class="precoo">R$ {item.preco}</h2>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              </div>
+          
+        ))}
+        </body>
       
     </section>
   );
