1a06d9e24aaf7703d1a316163f5b500cecff019d
Ivo Bueno Gouveia <ivo.buenogouveia@gmail.com>
2023-11-22 19:44:09 -0300

    1

--- site/src/pages/app/index.js
+++ site/src/pages/app/index.js
@@ -2,6 +2,16 @@ import { Link } from 'react-router-dom';
 import './index.scss';
 import { useState, useEffect, useRef  } from 'react';
 import axios from 'axios';
+import { register } from 'swiper/element/bundle'
+
+register();
+import 'swiper/scss';
+import 'swiper/scss/navigation';
+import 'swiper/scss/pagination';
+import 'swiper/scss/scrollbar';
+
+import {Swiper, SwiperSlide} from 'swiper/react';
+
 
 
 export default  function App() {
@@ -44,6 +54,7 @@ export default  function App() {
         </Link>
       </header>
 
+<<<<<<< Updated upstream
         <body className='home'>
         {jogosFiltrados.map((item) => (
            <Link to={`/produto/${item.id}`}>
@@ -62,7 +73,37 @@ export default  function App() {
                                     <h2 class="precoo">R$ {item.preco}</h2>
                                 </div>
                             </div>
+=======
+      
+      <div>
+        <ul>
+          {jogosFiltrados.map((item) => (
+            <li key={item.id}>
+              <div className="jogo">
+                <img src={`http://20.197.242.211:5000/${item.imagem}`} className="stock" alt="Imagem do Jogo"/>
+                <h1 className="nome">{item.nome}</h1>
+                <h1 className="preco">R$ {item.preco}</h1>
+              </div>
+            </li>
+          ))}
+        </ul>
+        </div>
+      
+      <body className='home'>
+      <div class="slider-container">
+        <div class="slide-content">
+            <div class="card-wrapper">
+                 <div class="card">
+                    <div class="image-content">
+                        <span class="overlay"></span>
+                      <Swiper>
+                        <SwiperSlide>
+                         <div class="card-image">
+                            <img src="/assets/images/metalslug.webp" alt="" class="card-img"/>
+>>>>>>> Stashed changes
                         </div>
+                        </SwiperSlide>
+                        </Swiper>
                     </div>
             </div>
         </Link>
